d0ba030606c2d1226e0d4b332e1000d6955efd1e
Jai Mahajan <jai16154@iiitd.ac.in>
2018-04-07 00:28:52 +0530

    ch
    c

---
+++ SoundRecorder-master/app/src/main/java/com/danielkim/soundrecorder/RecordingService.java
@@ -0,0 +1,226 @@
+package com.danielkim.soundrecorder;
+
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.app.Service;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.media.MediaRecorder;
+import android.os.Environment;
+import android.os.IBinder;
+import android.preference.PreferenceManager;
+import android.support.v4.app.NotificationCompat;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.danielkim.soundrecorder.activities.MainActivity;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Locale;
+import java.util.Timer;
+import java.util.TimerTask;
+
+/**
+ * Created by Daniel on 12/28/2014.
+ */
+public class RecordingService extends Service {
+
+    private static final String LOG_TAG = "RecordingService";
+
+    private String mFileName = null;
+    private String mFilePath = null;
+    private String mFileName2 = null;
+    private String mFilePath2 = null;
+    private MediaRecorder mRecorder = null;
+    private MediaRecorder mRecorder2 = null;
+
+    private DBHelper mDatabase;
+
+    private long mStartingTimeMillis = 0;
+    private long mElapsedMillis = 0;
+    private int mElapsedSeconds = 0;
+    private OnTimerChangedListener onTimerChangedListener = null;
+    private static final SimpleDateFormat mTimerFormat = new SimpleDateFormat("mm:ss", Locale.getDefault());
+
+    private Timer mTimer = null;
+    private TimerTask mIncrementTimerTask = null;
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    public interface OnTimerChangedListener {
+        void onTimerChanged(int seconds);
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        mDatabase = new DBHelper(getApplicationContext());
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        startRecording();
+        return START_STICKY;
+    }
+
+    @Override
+    public void onDestroy() {
+        if (mRecorder != null) {
+            stopRecording();
+        }
+        if (mRecorder2 != null) {
+            stopRecording();
+        }
+        super.onDestroy();
+    }
+
+    public void startRecording() {
+        setFileNameAndPath();
+
+        mRecorder = new MediaRecorder();
+        mRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
+        mRecorder.setOutputFormat(MediaRecorder.OutputFormat.MPEG_4);
+        mRecorder.setOutputFile(mFilePath);
+        mRecorder.setAudioEncoder(MediaRecorder.AudioEncoder.AAC);
+        mRecorder.setAudioChannels(1);
+        mRecorder2 = new MediaRecorder();
+        mRecorder2.setAudioSource(MediaRecorder.AudioSource.CAMCORDER);
+        mRecorder2.setOutputFormat(MediaRecorder.OutputFormat.MPEG_4);
+        mRecorder2.setOutputFile(mFilePath2);
+        mRecorder2.setAudioEncoder(MediaRecorder.AudioEncoder.AAC);
+        mRecorder2.setAudioChannels(1);
+        if (MySharedPreferences.getPrefHighQuality(this)) {
+            mRecorder.setAudioSamplingRate(44100);
+            mRecorder.setAudioEncodingBitRate(192000);
+            mRecorder2.setAudioSamplingRate(44100);
+            mRecorder2.setAudioEncodingBitRate(192000);
+        }
+
+        try {
+            mRecorder.prepare();
+            mRecorder.start();
+          mRecorder2.prepare();
+            mRecorder2.start();
+            mStartingTimeMillis = System.currentTimeMillis();
+
+            //startTimer();
+            //startForeground(1, createNotification());
+
+        } catch (IOException e) {
+            Log.e(LOG_TAG, "prepare() failed");
+        }
+        try {
+
+            mRecorder2.prepare();
+            mRecorder2.start();
+
+            //startTimer();
+            //startForeground(1, createNotification());
+
+        } catch (IOException e) {
+            Log.e(LOG_TAG, "prepare() failed");
+        }
+    }
+
+    public void setFileNameAndPath(){
+        int count = 0;
+        File f;
+
+        do{
+            count++;
+
+            mFileName = getString(R.string.default_file_name)
+                    + "_" + (mDatabase.getCount() + count) + ".mp4";
+            mFilePath = Environment.getExternalStorageDirectory().getAbsolutePath();
+            mFilePath += "/SoundRecorder/" + mFileName;
+
+            f = new File(mFilePath);
+        }while (f.exists() && !f.isDirectory());
+
+        File f2;
+
+        do{
+            count++;
+
+            mFileName2 = getString(R.string.default_file_name)
+                    + "_" + (mDatabase.getCount() + count) + ".mp4";
+            mFilePath2 = Environment.getExternalStorageDirectory().getAbsolutePath();
+            mFilePath2 += "/SoundRecorder/" + mFileName2;
+
+            f = new File(mFilePath2);
+        }while (f.exists() && !f.isDirectory());
+    }
+
+    public void stopRecording() {
+        mRecorder.stop();
+        mRecorder2.stop();
+
+        mElapsedMillis = (System.currentTimeMillis() - mStartingTimeMillis);
+        mRecorder.release();
+        mRecorder2.release();
+
+        Toast.makeText(this, getString(R.string.toast_recording_finish) + " " + mFilePath, Toast.LENGTH_LONG).show();
+//        Toast.makeText(this, getString(R.string.toast_recording_finish) + " " + mFilePath2, Toast.LENGTH_LONG).show();
+
+        //remove notification
+        if (mIncrementTimerTask != null) {
+            mIncrementTimerTask.cancel();
+            mIncrementTimerTask = null;
+        }
+
+        mRecorder = null;
+        mRecorder2 = null;
+
+        try {
+            mDatabase.addRecording(mFileName, mFilePath, mElapsedMillis);
+
+        } catch (Exception e){
+            Log.e(LOG_TAG, "exception", e);
+        }
+        try {
+            mDatabase.addRecording(mFileName2, mFilePath2, mElapsedMillis);
+
+        } catch (Exception e){
+            Log.e(LOG_TAG, "exception", e);
+        }
+    }
+
+    private void startTimer() {
+        mTimer = new Timer();
+        mIncrementTimerTask = new TimerTask() {
+            @Override
+            public void run() {
+                mElapsedSeconds++;
+                if (onTimerChangedListener != null)
+                    onTimerChangedListener.onTimerChanged(mElapsedSeconds);
+                NotificationManager mgr = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+                mgr.notify(1, createNotification());
+            }
+        };
+        mTimer.scheduleAtFixedRate(mIncrementTimerTask, 1000, 1000);
+    }
+
+    //TODO:
+    private Notification createNotification() {
+        NotificationCompat.Builder mBuilder =
+                new NotificationCompat.Builder(getApplicationContext())
+                        .setSmallIcon(R.drawable.ic_mic_white_36dp)
+                        .setContentTitle(getString(R.string.notification_recording))
+                        .setContentText(mTimerFormat.format(mElapsedSeconds * 1000))
+                        .setOngoing(true);
+
+        mBuilder.setContentIntent(PendingIntent.getActivities(getApplicationContext(), 0,
+                new Intent[]{new Intent(getApplicationContext(), MainActivity.class)}, 0));
+
+        return mBuilder.build();
+    }
+}
+
+
